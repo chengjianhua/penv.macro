0bb8a2bb64a7566a8688c78831c4eb7a0ee48724
cbbfcd <2890636389@qq.com>
2019-12-30 23:23:13 +0800

    use regexp for multiple keys

--- src/macro.js
+++ src/macro.js
@@ -14,7 +14,11 @@ function envVariableMacro({references, babel: {types: t}, config}) {
     const matchedPropertyPath = argumentPath
       .get('properties')
       .find(propertyPath => {
-        const keyName = propertyPath.get('key').node.name
+        const node = propertyPath.get('key').node
+        if (t.isRegExpLiteral(node))
+          return new RegExp(node.pattern).test(targetEnv)
+
+        const keyName = node.name
 
         return keyName === targetEnv
       })
